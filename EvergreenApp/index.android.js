75eb3a70cc070a5ec33579c0de8260da70818fc5
CurtisKreidler <kreidler@udel.edu>
2017-06-13 11:27:59 -0400

    firebase added brokens states

--- EvergreenApp/index.android.js
+++ EvergreenApp/index.android.js
@@ -11,6 +11,7 @@ import { StackNavigator } from 'react-navigation';
 import * as firebase from 'firebase';
 
 export default class LoginScreen extends Component {
+
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -30,11 +31,48 @@ export default class LoginScreen extends Component {
     }
   }
 
+  const firebaseConfig = {
+  apiKey: "AIzaSyDIQMP6yMBrKuwDvtRaSmhwYMMZC3FyqpY",
+  authDomain: "smart-garden-ca02a.firebaseapp.com",
+  databaseURL: "https://smart-garden-ca02a.firebaseio.com",
+  storageBucket: "smart-garden-ca02a.appspot.com",
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  // class sensorData extends Component {
+  //   render(){
+  //     return(
+  //       <div style={styles.dataBlock}>
+  //         <p style={{color:'#ffffff'}}>{this.props.data}</p>
+  //       </div>
+  //     );
+  //   }
+  // }
+
   class GardenScreen extends Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        sensorData: '' ,
+      };
+    }
+//need to add snapshot and updating
+    componentDidMount(){
+      firebase.database().ref('Sensors/sensor1').on('value', function(snapshot) {
+        this.setState({
+          sensorData: snapshot.child("sensorData").val(),
+        });
+      });
+    }
+
     render() {
+
       return (
         <View style={styles.container}>
           <Text style={{color:'#ffffff', fontSize:30}}>working</Text>
+          <Text>{this.state.sensorData}</Text>
         </View>
       );
     }
@@ -47,6 +85,13 @@ export default class LoginScreen extends Component {
       alignItems: 'center',
       justifyContent: 'center',
       padding: 10,
+    },
+    dataBlock:{
+      backgroundColor: 'blue',
+      display: 'inline-block',
+      height: 50,
+      width: 50,
+      textAlign: 'center',
     },
   });
 
