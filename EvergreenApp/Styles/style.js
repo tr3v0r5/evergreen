c1a3ee8a84d01886fbc6e7e70249015a5970a250
CurtisKreidler <kreidler@udel.edu>
2017-06-14 14:09:56 -0400

    seperate style and child components

---
+++ EvergreenApp/Styles/style.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(52,180,67)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  dataBlock:{
+    backgroundColor: 'blue',
+    height: 75,
+    width: 75,
+  },
+});
+
+module.exports = styles;
